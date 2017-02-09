da8d2458e29c9d3542ce200e5c89e6e5f0f7096c
Jsantiago2589 <josephasantiago25@gmail.com>
2017-02-08 20:36:26 -0500

    Add files via upload

---
+++ assignment 1.py
@@ -0,0 +1,6 @@
+dog =float (input("What is your dog's age"))
+a= (dog)
+b= (dog*7)
+print ("This is your dog's age in 'human' years", a)
+print ("Your dog's age in 'dog' years is", b)
+'''this simple program shows your dogs age in dog years. (assignment 1)'''
---
+++ assignment 3.py
@@ -0,0 +1,7 @@
+name= input("what is your name")
+print (" Nice to meet you",name)
+color= input("What is your favorite color")
+print (color,"That's a nice color")
+food= input("What's your favorite food")
+print (food,"Sounds yummy")
+question= input("do you have to go")
